7117957987143c8815ac764199557748b7b108d1
Aadarsha Jha <153203025+Er7890@users.noreply.github.com>
2025-12-26 14:41:15 +0545

    A small correction on permissions.py

--- complaints/permissions.py
+++ complaints/permissions.py
@@ -9,7 +9,7 @@ class IsOwnerOrAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-            
+            return True
         return obj.created_by == request.user or request.user.is_superuser
 
 
